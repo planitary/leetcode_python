589a8065570f3be1ca60737a73ff27c7b25ea0c7
planitary <853652156@qq.com>
2023-11-22 22:08:19 +0800

    20231122 leetcode151

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -5,9 +5,20 @@ def fib(n):
     return fib(n - 1) + fib (n - 2)
 
 if __name__ == "__main__":
-    before = int(time.time() * 1000)
-    print(before)
-    time.sleep(3)
-    after = int(time.time() * 1000)
-    print(after)
-    print((after - before) // 1000)
+
+    s = "the blue sky"
+    # s = s[::-1]
+    left,right = 0,1
+    s[2],s[1] = s[1],s[2]
+
+    # while right < len(s):
+    #     if s[right] == " ":
+    #         while left < right - 1:
+    #             s[left],s[right - 1] = s[right - 1],s[left]
+    #             left += 1
+    #             right -= 1
+    #         left = right + 1
+    #         right = left + 1
+    #     right += 1
+    # print(s)
+
